5440f82c2557a16753edc97829e5686d88381abd
jcrnieto <jcrnietos@gmail.com>
2025-08-12 21:05:17 -0300

    add detailProduct

--- src/components/PublicStore/page/AllProductsByCategory.jsx
+++ src/components/PublicStore/page/AllProductsByCategory.jsx
@@ -6,7 +6,7 @@ import axios from 'axios';
 const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
 const AllProductsByCategory = () => {
-  const { userId, categoryId } = useParams();
+  const { slug, userId, categoryId } = useParams();
   const navigate = useNavigate();
 
   const [products, setProducts] = useState([]);
@@ -40,7 +40,7 @@ const AllProductsByCategory = () => {
     <div className="p-4">
       {/* Botón volver */}
       <button
-        className="flex items-center text-blue-600 mb-4"
+        className="flex items-center text-black-600 mb-4"
         onClick={() => navigate(-1)}
       >
         <ArrowLeft className="w-5 h-5 mr-2" /> 
@@ -50,22 +50,25 @@ const AllProductsByCategory = () => {
 
       {/* Grid de productos */}
       {products.length > 0 ? (
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
           { products.map((prod) => (
-            <div
-              key={prod.id}
-              className="bg-white border rounded-xl shadow hover:shadow-lg transition-shadow"
+          <div 
+            key={prod.id} 
+            className="rounded-xl overflow-hidden mt-7 shadow"
+            onClick={() => navigate(`/store/${slug}/${userId}/product/${prod.id}`)}
             >
+            <div className="aspect-square">
               <img
                 src={prod.images_url?.[0]}
                 alt={prod.name}
-                className="w-full h-48 object-cover rounded-t-xl"
+                className="w-full h-full object-cover"
               />
-              <div className="p-4">
-                <h3 className="text-lg font-semibold">{prod.name}</h3>
-                <p className="text-gray-600">${prod.price}</p>
-              </div>
             </div>
+            <div className="p-4">
+              <h3 className="text-lg font-semibold">{prod.name}</h3>
+              <p className="text-gray-600">${prod.price}</p>
+            </div>
+          </div>
           ))}
         </div>
       ) : (
